e6f56868f57c59d0d344db53947e369ad5fdd300
Greta Cutulenco <gcutulenco@gmail.com>
2015-08-12 23:38:38 -0400

    add the other two views and config fetching routes

--- public/javascripts/rithm.js
+++ public/javascripts/rithm.js
@@ -21,3 +21,20 @@ rithmApp.controller('ConfigController', function($scope, $http) {
     $scope.invocator = data;
   });
 });
+
+rithmApp.controller('PluginController', function($scope, $http) {
+});
+
+rithmApp.controller('PipeController', function($scope, $http) {
+  $http.get('/config/format').success(function(data) {
+    $scope.format = data;
+  });
+
+  $http.get('/config/evaluator').success(function(data) {
+    $scope.evaluator = data;
+  });
+
+  $http.get('/config/invocator').success(function(data) {
+    $scope.invocator = data;
+  });
+});
